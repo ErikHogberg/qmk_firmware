92e99b614e35a266c4e81036853e7830868732b5
Erik Högberg <erikh_93@hotmail.com>
2021-10-22 01:03:38 +0200

    ohcr preonic changed startup song and mouse speed

--- keyboards/preonic/keymaps/ohcr/config.h
+++ keyboards/preonic/keymaps/ohcr/config.h
@@ -4,8 +4,8 @@
 #include "config_common.h"
 
 #ifdef AUDIO_ENABLE
-    #define STARTUP_SONG SONG(ONE_UP_SOUND)
-    // #define STARTUP_SONG SONG(PREONIC_SOUND)
+    // #define STARTUP_SONG SONG(ONE_UP_SOUND)
+    #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
     #define DEFAULT_LAYER_SONGS { SONG(QWERTY_SOUND), \
